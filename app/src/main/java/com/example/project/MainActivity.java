eb25ac6feaff796fe6ef95e2c0805708fe746fe8
a22husba <a22husba@student.his.se>
2024-10-25 09:18:03 +0200

    Main ac..

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView sharedresult;
+    private Button button;
+    private final String key = String.valueOf("viewData");
+    private SharedPreferences myPreferenceREf;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +24,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        button = findViewById(R.id.sendButton);
+        sharedresult = findViewById(R.id.textViewAccount);
+
+        myPreferenceREf = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceREf.edit();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSecondActivity;
+            }
+        });
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("a22husba", "startActivity");
+        String defaultValue ="Nothing shared yet";
+        SharedPreferences sharedPref = getSharedPreferences(key, MODE_PRIVATE);
+        sharedresult.setText(sharedPref.getString(key, defaultValue));
+    }
+
+
 
 }
